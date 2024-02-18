0eae1fad0cd48d4f4537f9881cf2ba118a31db32
SelinaM04 <may162@newschool.edu>
2024-02-17 23:49:33 -0500

    2.17

--- entry-1/style/script.js
+++ entry-1/style/script.js
@@ -1,22 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
     const links = document.querySelectorAll('.link-container .link');
+    const titlePage = document.querySelector('.titlepage');
 
     links.forEach(link => {
         link.addEventListener('mouseenter', function(event) {
-           
             addBlurToOtherLinks(event);
-          
             scaleUpLink(event);
         });
         link.addEventListener('mouseleave', function(event) {
-            
             removeBlurFromLinks();
-           
             resetLinkScale(event);
         });
     });
-
-
+    
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+    
+    slider.oninput = function() {
+      output.innerHTML = this.value;
+    }
 
     function addBlurToOtherLinks(event) {
         links.forEach(link => {
