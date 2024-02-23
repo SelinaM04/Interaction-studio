81a1f984dc20bc58f3eddbfd32cc189f24c62220
SelinaM04 <may162@newschool.edu>
2024-02-23 11:20:54 -0500

    2.23

--- entry-2/style/script.js
+++ entry-2/style/script.js
@@ -4,20 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const thirdContent = document.getElementById('third-Content');
     const mask = document.querySelector('.mask');
 
-    // Hide the second and third paragraphs initially
     secondContent.style.display = 'none';
     secondContent.style.opacity = '0';
     thirdContent.style.display = 'none';
     thirdContent.style.opacity = '0';
 
     function fadeOut(element, callback) {
-        let op = 1; // initial opacity
+        let op = 1; 
         const timer = setInterval(() => {
             if (op <= 0.01) {
                 clearInterval(timer);
                 element.style.opacity = '0';
                 element.style.display = 'none';
-                callback && callback(); // Execute callback after fade out if provided
+                callback && callback(); 
             } else {
                 element.style.opacity = op;
                 op -= op * 0.1;
@@ -27,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function fadeIn(element) {
         element.style.display = 'block';
-        let op = 0.01; // initial low opacity for fade in
+        let op = 0.01; 
         const timer = setInterval(() => {
             if (op >= 1) {
                 clearInterval(timer);
@@ -39,11 +38,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 10);
     }
 
-    // Add click event listeners to paragraphs for fading
     content.addEventListener('click', () => fadeOut(content, () => fadeIn(secondContent)));
     secondContent.addEventListener('click', () => fadeOut(secondContent, () => fadeIn(thirdContent)));
 
-    // Adjust the mask effect based on mouse movement
     document.addEventListener('pointermove', (event) => {
         const x = parseInt(event.clientX / window.innerWidth * 100);
         const y = parseInt(event.clientY / window.innerHeight * 100);
